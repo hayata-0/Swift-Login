b5f450ff68626e0393b568e2b6f66e4f74cb75ea
hayata-0 <soccerkozo8814@docomo.ne.jp>
2022-02-08 01:28:44 +0900

    ログインボタンを押した時の処理,ログインされていた時の処理を追加

--- EmailPasswordLoginSwift/ViewController.swift
+++ EmailPasswordLoginSwift/ViewController.swift
@@ -22,7 +22,12 @@ class ViewController: UIViewController {
         let emailField = UITextField()
         emailField.placeholder = "メールアドレス"
         emailField.layer.borderWidth = 1
+        //英字入力で最初の文字を大文字から小文字に変更
+        emailField.autocapitalizationType = .none
         emailField.layer.borderColor = UIColor.black.cgColor
+        emailField.backgroundColor = .white
+        emailField.leftViewMode = .always
+        emailField.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 5, height: 0))
         return emailField
     }()
     
@@ -33,6 +38,9 @@ class ViewController: UIViewController {
         //文字を入力する際に文字が見えないようにする
         passwordField.isSecureTextEntry = true
         passwordField.layer.borderColor = UIColor.black.cgColor
+        passwordField.backgroundColor = .white
+        passwordField.leftViewMode = .always
+        passwordField.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 5, height: 0))
         return passwordField
     }()
     
@@ -43,6 +51,14 @@ class ViewController: UIViewController {
         button.setTitle("ログイン", for: .normal)
         return button
     }()
+    
+    private let signOutButton:UIButton = {
+        let signOutButton = UIButton()
+        signOutButton.backgroundColor = .systemGreen
+        signOutButton.setTitleColor(.white, for: .normal)
+        signOutButton.setTitle("ログアウト", for: .normal)
+        return signOutButton
+    }()
 
     
     override func viewDidLoad() {
@@ -51,8 +67,35 @@ class ViewController: UIViewController {
         view.addSubview(emailField)
         view.addSubview(passwordField)
         view.addSubview(button)
-        
+        view.backgroundColor = .systemOrange
         button.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
+        
+        if FirebaseAuth.Auth.auth().currentUser != nil {
+            label.isHidden = true
+            button.isHidden = true
+            emailField.isHidden = true
+            passwordField.isHidden = true
+            
+            view.addSubview(signOutButton)
+            signOutButton.frame = CGRect(x: 20, y: 150, width: view.frame.size.width-40, height: 52)
+            signOutButton.addTarget(self, action: #selector(logOutTapped), for: .touchUpInside)
+        }
+    }
+    
+    @objc private func logOutTapped(){
+        do{
+            try FirebaseAuth.Auth.auth().signOut()
+            
+            label.isHidden = false
+            button.isHidden = false
+            emailField.isHidden = false
+            passwordField.isHidden = false
+            signOutButton.removeFromSuperview()
+        }catch{
+            print("サインアウトできませんでした")
+            
+            
+        }
     }
     
     override func viewDidLayoutSubviews() {
@@ -79,7 +122,9 @@ class ViewController: UIViewController {
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        emailField.becomeFirstResponder()
+        if FirebaseAuth.Auth.auth().currentUser == nil {
+            emailField.becomeFirstResponder()
+        }
     }
     
     @objc private func didTapButton() {
@@ -89,7 +134,52 @@ class ViewController: UIViewController {
                   print("エラー")
                   return
               }
-              
+        
+        FirebaseAuth.Auth.auth().signIn(withEmail: email, password: password, completion: {[weak self] result, error in
+            guard let strongSelf = self else {
+                return
+            }
+            guard error == nil else{
+                strongSelf.showCreateAccount(email: email, password: password)
+                return
+            }
+            
+            print("サインインをしてください")
+            strongSelf.label.isHidden = true
+            strongSelf.emailField.isHidden = true
+            strongSelf.passwordField.isHidden = true
+            strongSelf.button.isHidden = true
+            
+            strongSelf.emailField.resignFirstResponder()
+            strongSelf.passwordField.resignFirstResponder()
+        })
+        
+    }
+    
+    func showCreateAccount(email:String,password:String) {
+        let alert = UIAlertController(title: "アカウントを作成してください", message: "アカウントを作成しましょう", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Continue", style: .default, handler: {_ in
+            FirebaseAuth.Auth.auth().createUser(withEmail: email, password: password, completion: {[weak self] result, error in
+                guard let strongSelf = self else {
+                    return
+                }
+                guard error == nil else{
+                    print("アカウントを作成しました")
+                    return
+                }
+                
+                print("サインインをしてください")
+                strongSelf.label.isHidden = true
+                strongSelf.emailField.isHidden = true
+                strongSelf.passwordField.isHidden = true
+                strongSelf.button.isHidden = true
+                
+                strongSelf.emailField.resignFirstResponder()
+                strongSelf.passwordField.resignFirstResponder()
+            })
+        }))
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: {_ in}))
+        present(alert,animated: true)
     }
 
 
